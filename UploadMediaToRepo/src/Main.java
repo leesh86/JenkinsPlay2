e749fb65f3a485373764346fed813dd7ceb84b4a
Lee Shoham <lees@perfectomobile.com>
2017-04-27 14:04:07 +0300

    add the upload media project to repo

---
+++ UploadMediaToRepo/src/Main.java
@@ -0,0 +1,119 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * 
+ */
+
+/**
+ * @author Lee Shoham
+ * @date Apr 27, 2017
+ */
+public class Main {
+	
+	private static final String HTTPS = "https://";
+	private static final String MEDIA_REPOSITORY = "/services/repositories/media/";
+	private static final String UPLOAD_OPERATION = "operation=upload&overwrite=true";
+	private static final String UTF_8 = "UTF-8";
+
+
+	public static void main(String[] args) throws IOException {
+		uploadMedia("ps.perfectomobile.com", "lees@perfectomobile.com", "Welcome1701", "C:\\Users\\lees\\Downloads\\EriBank.apk", "PRIVATE:applications/EriBank.apk");
+	}
+	
+	/**
+	 * Uploads a file to the media repository.
+	 * Example:
+	 * uploadMedia("demo.perfectomobile.com", "john@perfectomobile.com", "123456", "C:\\test\\ApiDemos.apk", "PRIVATE:apps/ApiDemos.apk");
+	 */
+	private static void uploadMedia(String host, String user, String password, String path, String repositoryKey) throws IOException {
+		File file = new File(path);
+		byte[] content = readFile(file);
+		uploadMedia(host, user, password, content, repositoryKey);
+	}
+	
+	/**
+	 * Uploads content to the media repository.
+	 * Example:
+	 * uploadMedia("demo.perfectomobile.com", "john@perfectomobile.com", "123456", content, "PRIVATE:apps/ApiDemos.apk");
+	 */
+	public static void uploadMedia(String host, String user, String password, byte[] content, String repositoryKey) throws UnsupportedEncodingException, MalformedURLException, IOException {
+		if (content != null) {
+			String encodedUser = URLEncoder.encode(user, "UTF-8");
+			String encodedPassword = URLEncoder.encode(password, "UTF-8");
+			String urlStr = HTTPS + host + MEDIA_REPOSITORY + repositoryKey + "?" + UPLOAD_OPERATION + "&user=" + encodedUser + "&password=" + encodedPassword;
+			URL url = new URL(urlStr);
+
+			sendRequest(content, url);
+		}
+	}
+	
+	private static byte[] readFile(File path) throws FileNotFoundException, IOException {
+		int length = (int)path.length();
+		byte[] content = new byte[length];
+		InputStream inStream = new FileInputStream(path);
+		try {
+			inStream.read(content);
+		}
+		finally {
+			inStream.close();
+		}
+		return content;
+	}
+	
+	private static void sendRequest(byte[] content, URL url) throws IOException {
+		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		connection.setDoOutput(true);
+		connection.setRequestProperty("Content-Type", "application/octet-stream");
+		connection.connect();
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		outStream.write(content);
+		outStream.writeTo(connection.getOutputStream());
+		outStream.close();
+		int code = connection.getResponseCode();
+		if (code > HttpURLConnection.HTTP_OK) {
+			handleError(connection);
+		}
+	}
+	
+	private static void handleError(HttpURLConnection connection) throws IOException {
+		String msg = "Failed to upload media.";
+		InputStream errorStream = connection.getErrorStream();
+		if (errorStream != null) {
+			InputStreamReader inputStreamReader = new InputStreamReader(errorStream, UTF_8);
+			BufferedReader bufferReader = new BufferedReader(inputStreamReader);
+			try {
+				StringBuilder builder = new StringBuilder();
+				String outputString;
+				while ((outputString = bufferReader.readLine()) != null) {
+					if (builder.length() != 0) {
+						builder.append("\n");
+					}
+					builder.append(outputString);
+				}
+				String response = builder.toString();
+				msg += "Response: " + response;
+			}
+			finally {
+				bufferReader.close();
+			}
+		}
+		throw new RuntimeException(msg);
+	}
+
+
+
+
+
+}
